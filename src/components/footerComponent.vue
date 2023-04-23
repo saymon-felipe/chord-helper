d211cec167646b5b557bf5623ee46a1589234e5c
Saymon Felipe <linnubr@gmail.com>
2023-04-23 15:40:12 -0300

    Ano dinamico no footer

--- src/components/footerComponent.vue
+++ src/components/footerComponent.vue
@@ -1,14 +1,27 @@
 <template>
     <footer class="w-100 d-flex justify-content-center py-4">
         <div class="text-center">
-            <p>Copyright © 2022 - Chord helper</p>
+            <p>Copyright © {{ year }} - Chord helper</p>
             <span>By Rabsystems</span>
         </div>
     </footer>    
 </template>
 <script>
 export default {
-    name: "footerComponent"
+    name: "footerComponent",
+    data() {
+        return {
+            year: null
+        }
+    },
+    methods: {
+        fillYear: function () {
+            this.year = new Date().getFullYear();
+        }
+    },
+    mounted: function () {
+        this.fillYear();
+    }
 }
 </script>
 <style scoped>
